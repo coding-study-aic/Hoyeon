982a35dee2d9600cc5a76a109aa8a8bb8b035745
ghdus819 <ghdus819@gmail.com>
2022-01-17 17:04:11 +0900

    Greedy

---
+++ 220117_BaekJoon_11508.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int inputNum;
+	cin >> inputNum;
+
+	priority_queue<int> pricePq;
+
+	for (int i = 0; i < inputNum; i++) {
+		int inputPrice;
+		cin >> inputPrice;
+		pricePq.push(inputPrice);
+	}
+
+	int priceSum = 0;
+
+	for (int i = 0; i < inputNum; i++) {
+		if (i % 3 == 2) {
+			pricePq.pop();
+			continue;
+		}
+
+		priceSum += pricePq.top();
+		pricePq.pop();
+	}
+
+	cout << priceSum;
+
+	return 0;
+}
