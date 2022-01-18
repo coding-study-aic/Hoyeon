ed0efa8567e4d64aff5d2dc211e5116d783b34ae
ghdus819 <ghdus819@gmail.com>
2022-01-18 22:01:53 +0900

    Greedy

---
+++ 220118_BaekJoon_11399.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int inputNum;
+	cin >> inputNum;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	for (int i = 0; i < inputNum; i++) {
+		int time;
+		cin >> time;
+		pq.push(time);
+	}
+
+	int partSum = 0, totalSum = 0;
+
+	for (int i = 0; i < inputNum; i++) {
+		partSum += pq.top();
+		totalSum += partSum;
+		pq.pop();
+	}
+
+	cout << totalSum;
+
+	return 0;
+}
