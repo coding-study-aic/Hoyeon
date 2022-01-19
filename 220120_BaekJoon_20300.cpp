243bdfe65e8ec4b55120d8fb89efb322ac59cb9d
ghdus819 <ghdus819@gmail.com>
2022-01-20 04:56:32 +0900

    Greedy

---
+++ 220120_BaekJoon_20300.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int inputNum;
+	cin >> inputNum;
+
+	vector<long long int> vec;
+
+	for (int i = 0; i < inputNum; i++) {
+		long long int input;
+		cin >> input;
+		vec.push_back(input);
+	}
+
+	sort(vec.begin(), vec.end());
+
+	long long int minRes = vec[inputNum - 1];
+	inputNum = (inputNum / 2) * 2;
+
+	for (int i = 0; i < inputNum; i++) {
+		long long int sum = vec[i] + vec[inputNum - i - 1];
+		minRes = max(minRes, sum);
+	}
+
+	cout << minRes;
+
+	return 0;
+}
---
+++ 220120_BaekJoon_20365.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int inputLen;
+	cin >> inputLen;
+
+	string inputStr;
+	cin >> inputStr;
+
+	inputStr = '0' + inputStr;
+
+	map<char, int> continueMap;
+	continueMap['B'] = continueMap['R'] = 0;
+
+	for (int i = 1; i <= inputLen; i++) {
+		if (inputStr[i] != inputStr[i - 1]) {
+			continueMap[inputStr[i]]++;
+		}
+	}
+
+	int minLen = min(continueMap['B'], continueMap['R']);
+
+	cout << minLen + 1;
+
+	return 0;
+}
