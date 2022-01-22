a44d3d308d739cf3e057100fc49c5064cedac5a3
ghdus819 <ghdus819@gmail.com>
2022-01-22 21:25:37 +0900

    Greedy

---
+++ 220122_BaekJoon_1715.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int inputNum;
+	cin >> inputNum;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	for (int i = 0; i < inputNum; i++) {
+		int num;
+		cin >> num;
+		pq.push(num);
+	}
+
+	int resSum = 0;
+
+	while (pq.size() > 1) {
+		int tempSum = pq.top();
+		pq.pop();
+
+		tempSum += pq.top();
+		pq.pop();
+
+		resSum += tempSum;
+		pq.push(tempSum);
+	}
+
+	cout << resSum;
+
+	return 0;
+}
