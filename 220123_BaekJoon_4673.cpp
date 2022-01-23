1bfdd01acef198ee9ae6f6427550dc78fce22a78
ghdus819 <ghdus819@gmail.com>
2022-01-24 00:00:21 +0900

    Math

---
+++ 220123_BaekJoon_4673.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#define MAX_NUM 10000 + 36
+#define MAX_SIZE 10000 + 1
+using namespace std;
+
+int main() {
+	bool resArr[MAX_NUM] = { false };
+
+	for (int i = 1; i < MAX_SIZE; i++) {
+		int target = i;
+		int nonSelf = target;
+
+		while (target > 0) {
+			nonSelf += target % 10;
+			target /= 10;
+		}
+
+		resArr[nonSelf] = true;
+	}
+
+
+	for (int i = 1; i < MAX_SIZE; i++) {
+		if (!resArr[i]) {
+			cout << i << "\n";
+		}
+	}
+
+	return 0;
+}
