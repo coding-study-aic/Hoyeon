a7d4c4e5c2cff9e5817f90a80abd2a2b139d5b5b
ghdus819 <ghdus819@gmail.com>
2022-02-01 23:50:23 +0900

    DP

---
+++ 220201_BaekJoon_2293.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#define MAX_TYPE 100 + 1
+#define MAX_AMOUNT 10000 + 1
+using namespace std;
+
+int main() {
+	int typeNum, totalAmount;
+	cin >> typeNum >> totalAmount;
+
+	int typeArr[MAX_TYPE], dp[MAX_AMOUNT] = { 0 };
+
+	for (int i = 0; i < typeNum; i++) {
+		cin >> typeArr[i];
+	}
+
+	dp[0] = 1;
+
+	for (int i = 0; i < typeNum; i++) {
+		for (int j = 1; j <= totalAmount; j++) {
+			if (j >= typeArr[i]) {
+				dp[j] += dp[j - typeArr[i]];
+			}
+		}
+	}
+
+	cout << dp[totalAmount];
+
+	return 0;
+}
