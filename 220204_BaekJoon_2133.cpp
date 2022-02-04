ac316f972c87f1b33d6b4fba01ea06e32ba370a7
ghdus819 <ghdus819@gmail.com>
2022-02-04 23:47:39 +0900

    DP

---
+++ 220204_BaekJoon_2133.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#define MAX_SIZE 30 + 1
+using namespace std;
+
+int main() {
+	int sizeNum;
+	cin >> sizeNum;
+
+	int dp[MAX_SIZE] = { 0 };
+	dp[0] = 1, dp[2] = 3;
+
+	for (int i = 3; i <= sizeNum; i++) {
+		dp[i] = dp[i - 2] * 3;
+
+		for (int j = 4; j <= i; j += 2) {
+			dp[i] += dp[i - j] * 2;
+		}
+	}
+
+	cout << dp[sizeNum];
+
+	return 0;
+}
