991fe4d14a2fcfc61bc505ff00ce56e9d7e1aaeb
ghdus819 <ghdus819@gmail.com>
2022-02-08 00:10:25 +0900

    Map

---
+++ 220207_BaekJoon_11652.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main() {
+	int inputNum;
+	cin >> inputNum;
+	
+	map<long long, int> inputMap;
+	int maxCnt = 0;
+	long long maxInput;
+
+	for (int i = 0; i < inputNum; i++) {
+		long long input;
+		cin >> input;
+
+		inputMap[input]++;
+
+		if (inputMap[input] > maxCnt) {
+			maxCnt = inputMap[input];
+			maxInput = input;
+		}
+
+		else if (inputMap[input] == maxCnt) {
+			maxInput = min(input, maxInput);
+		}
+	}
+
+	cout << maxInput;
+
+	return 0;
+}
